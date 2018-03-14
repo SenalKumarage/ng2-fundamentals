f7024b43099ce795d33897dc13785dc8f76b687f
Senal Kumarage <senalkumarage@gmail.com>
2018-03-14 16:50:42 +0800

    Getting started with routes

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,13 +6,20 @@ import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnai
 import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
     declarations: [
         EventsAppComponent,
         EventsListComponent,
         EventThumbnailComponent,
+        EventDetailsComponent,
         NavBarComponent
     ],
     providers: [
