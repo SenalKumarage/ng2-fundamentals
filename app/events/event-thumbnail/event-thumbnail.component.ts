10ee0ad627015d9de8f2ce1fc94c1ff21ec0e60f
Senal Kumarage <senalkumarage@gmail.com>
2018-03-14 15:10:56 +0800

    Styling with class binding

--- app/events/event-thumbnail/event-thumbnail.component.ts
+++ app/events/event-thumbnail/event-thumbnail.component.ts
@@ -6,13 +6,18 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     <div class="well hoverwell thumbnail">
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div>Time: {{event?.time}}</div>
+        <div [class.green]="event?.time === '8:00 am'" [ngSwitch]="event?.time">
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>
         <div>Price: \${{event?.price}}</div>
-        <div>
+        <div [hidden]="!event?.location">
             <span>Locaton: {{event?.location?.address}}</span>
             <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
         </div>
-        <div>
+        <div *ngIf="event?.onlineUrl">
             Online URL: {{event?.onlineUrl}}
         </div>
     </div>
@@ -20,7 +25,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     styles: [`
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb; }
-        .thumbnail { min-height: 210px }
+        .thumbnail { min-height: 210px; }
+        .green { color: #003300 !important; }
+        .bold { font-weight: bold; }
     `]
 })
 export class EventThumbnailComponent {
