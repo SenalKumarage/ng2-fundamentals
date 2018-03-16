4fd1777222088dbdb8948ff8de48ff2097eea605
Senal Kumarage <senalkumarage@gmail.com>
2018-03-16 14:38:29 +0800

    Barrels and models

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    EventRouteActivator,
+    EventListResolver,
+    CreateEventComponent
+} from './events/index';
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list/events-list-resolver.service';
 
 @NgModule({
     imports: [
--- app/events/event-thumbnail/event-thumbnail.component.ts
+++ app/events/event-thumbnail/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { Event } from '../index'; 
 
 @Component({
     selector: 'event-thumbnail',
@@ -31,7 +32,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     `]
 })
 export class EventThumbnailComponent {
-    @Input() event: any;
+    @Input() event: Event;
     
     getStartTimeClass() {
         //Approach-1
